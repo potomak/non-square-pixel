30dc5825d6e53e51908074b5265b07b95a313f5f
Giovanni Cappellotto <potomak84@gmail.com>
2012-05-20 03:54:40 +0200

    Add non_square_pixel.rb Ruby script; add Gemfile

---
+++ non_square_pixel.rb
@@ -0,0 +1,40 @@
+require 'bundler'
+
+Bundler.setup
+Bundler.require
+
+include Magick
+
+puts "non square pixel"
+
+images = ImageList.new('images/test_1.jpg', 'images/test_2.png')
+
+images.each do |image|
+  rows = image.rows
+  cols = image.columns
+  pixels = []
+
+  rows.times do |i|
+    3.times do |n|
+      cols.times do |j|
+        pixel = image.pixel_color(i, cols - (j+1))
+
+        pixels << pixel.red
+        pixels << 0
+        pixels << 0
+
+        pixels << 0
+        pixels << pixel.green
+        pixels << 0
+
+        pixels << 0
+        pixels << 0
+        pixels << pixel.blue
+      end
+    end
+  end
+
+  Image.constitute(cols*3, rows*3, "RGB", pixels).write("images/non_square_#{File.basename(image.filename)}.png")
+end
+
+puts "done"
